b7f94231a9445c8642f5d189210b4f2510711aa8
Devender00999 <Devender00999@gmail.com>
2021-03-06 23:35:51 +0530

    Added Login Page

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,13 @@
 var signinBtn = document.getElementById("signinBtn");
 var signupBtn = document.getElementById("signupBtn");
+var register = document.getElementById("register");
+
 
 signinBtn.addEventListener("click", () => {
-    document.location.href = "./Login/index.html";
+    document.location.href = "./login/index.html";
 })
 
 signupBtn.addEventListener("click", () => {
-    var register = document.getElementById("register");
     register.style.opacity="1";
     register.style.visibility="visible";
     var body = document.getElementById("body");
@@ -14,12 +15,15 @@ signupBtn.addEventListener("click", () => {
     register.style.transform="opacity 50ms ease-in-out, visibility 0s linear 50ms";
 })
 
+var rcontent = document.getElementById("rContent");
 
-function hideOverlay(){
-    var register = document.getElementById("register");
+rcontent.addEventListener("click",()=>{
     register.style.opacity="0";
     register.style.visibility="hidden";
     var body = document.getElementById("body");
     body.style.filter="none";
     console.log("not Worlking");
+})
+function hideOverlay(){
+    
 }
