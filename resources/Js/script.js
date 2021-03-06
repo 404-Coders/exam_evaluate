32f835a608ae8c7ec1fb28b84d6ef9b2627bfb1f
KushdeepSingh <skushdeep5@gmail.com>
2021-03-06 16:50:24 +0530

    added login folder

---
+++ resources/Js/script.js
@@ -0,0 +1,10 @@
+var signinBtn = document.getElementById("signinBtn");
+var signupBtn = document.getElementById("signupBtn");
+
+signinBtn.addEventListener("click", () => {
+    document.location.href = "./Login/index.html";
+})
+
+signupBtn.addEventListener("click", () => {
+    document.location.href = "./Login/index.html";
+})
