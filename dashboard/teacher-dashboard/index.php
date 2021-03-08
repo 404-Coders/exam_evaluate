0ac929815c58701a1c3aba26647a4ce39d5aa2c0
KushdeepSingh <skushdeep5@gmail.com>
2021-03-09 02:48:49 +0530

    add class backend added

--- dashboard/teacher-dashboard/index.php
+++ dashboard/teacher-dashboard/index.php
@@ -127,11 +127,31 @@
     </section>
 
     <section class="modal_cobtainer" id="create_class" style="visibility: hidden;">
+    <!-- <section class="modal_cobtainer" id="create_class"> -->
         <div class="create__modal">
             <div class="create__modal__head">
                 <img class="register__cancel" onclick="hideModal('create_class')" src="../../resources/images/cancel.svg" alt="">
             </div>
-            <div class="create__modal__body"></div>
+            <div class="create__modal__body">
+                <div class="modal">
+                    <div class="modal_icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 477.873 477.873" style="enable-background:new 0 0 512 512" xml:space="preserve" class="">
+                            <path d="M392.533,238.937c-9.426,0-17.067,7.641-17.067,17.067V426.67c0,9.426-7.641,17.067-17.067,17.067H51.2    c-9.426,0-17.067-7.641-17.067-17.067V85.337c0-9.426,7.641-17.067,17.067-17.067H256c9.426,0,17.067-7.641,17.067-17.067    S265.426,34.137,256,34.137H51.2C22.923,34.137,0,57.06,0,85.337V426.67c0,28.277,22.923,51.2,51.2,51.2h307.2    c28.277,0,51.2-22.923,51.2-51.2V256.003C409.6,246.578,401.959,238.937,392.533,238.937z" fill="#ffffff" data-original="#000000" style="" class=""/>
+                            <path d="M458.742,19.142c-12.254-12.256-28.875-19.14-46.206-19.138c-17.341-0.05-33.979,6.846-46.199,19.149L141.534,243.937    c-1.865,1.879-3.272,4.163-4.113,6.673l-34.133,102.4c-2.979,8.943,1.856,18.607,10.799,21.585    c1.735,0.578,3.552,0.873,5.38,0.875c1.832-0.003,3.653-0.297,5.393-0.87l102.4-34.133c2.515-0.84,4.8-2.254,6.673-4.13    l224.802-224.802C484.25,86.023,484.253,44.657,458.742,19.142z" fill="#ffffff" data-original="#000000" style="" class=""/>
+                        </svg>
+                    </div>
+                    <div class="modal_heading">
+                        <p>Create Class</p>
+                    </div>
+                    <form action="../../resources/php/addClass.php" method="POST">
+                        <div class="modal_input">
+                            <input class="modal_input_box" type="text" id="class_name" name="class_name" required placeholder="Class Name">
+                            <input class="modal_input_box" type="text" id="full_sub_name" name="full_sub_name" required placeholder="Full Subject Name">
+                            <button type="submit" class="modal_input_submit primary-button" name="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
     <script src="../../resources/js/teacher-dashboard.js"></script>
---
+++ resources/php/addClass.php
@@ -0,0 +1,27 @@
+<?php
+    include "./connection.php";
+    
+    session_start();
+    $tea_id = $_SESSION['teaID'];
+    $class_name = strtoupper($_POST['class_name']);
+    $full_sub_name = ucwords($_POST['full_sub_name']);
+
+    $sub_name_words = explode(" ",$full_sub_name);
+    print_r($sub_name_words);
+
+    $sub_name = '';
+    for($i = 0; $i < count($sub_name_words); $i++){
+        $sub_name = $sub_name.$sub_name_words[$i][0];
+    }
+
+    $class_id = $class_name."-".$sub_name;
+
+    $query = mysqli_query($con, "INSERT INTO `exam_classes` VALUES (NULL, '$class_id', '$class_name', '$sub_name', '$full_sub_name', '$tea_id');");
+    if($query > 0){
+        header("location: ../../dashboard/teacher-dashboard/");
+    }
+    else{
+        echo "Not Added";
+        // Failed to add SWAL
+    }
+?>
